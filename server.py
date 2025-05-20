0e6ae3b91b2b3fccdfcfd12f2c4e505a72b99e8a
Anders strangstad <anders@strangstad.no>
2025-05-20 18:05:43 -0400

    added response display formating

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def detect_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
     
     emotion = emotion_detector(text_to_analyze)
-    return emotion
+    return f"For the given statement, the system response is 'anger': {emotion['anger']}, 'disgust': {emotion['disgust']}, 'fear': {emotion['fear']}, 'joy': {emotion['joy']}, 'sadness': {emotion['sadness']}, The dominant emotion is {emotion['dominant_emotion']}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

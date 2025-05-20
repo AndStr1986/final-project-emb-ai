0cc10a14e2475359c7d784e0a93ac35deebc7395
Anders <anders@strangstad.com>
2025-05-20 08:20:40 -0400

    added pars_response and find_dom func

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse: str) -> str:
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,13 @@ def emotion_detector(text_to_analyse: str) -> str:
     input_string = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json=input_string, headers=header)
-    return response.text
+    emotions = pars_response(response)
+    emotions["dominant_emotion"] = find_dominant_emotion(emotions)
+    return emotions
+
+def pars_response(response_text: str) -> dict:
+    emotions = response_text.json()["emotionPredictions"][0]["emotion"]
+    return emotions
+
+def find_dominant_emotion(emotions: dict) -> str:
+    return max(zip(emotions.values(), emotions.keys()))[1]

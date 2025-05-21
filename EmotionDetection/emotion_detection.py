a599d867b21f69f714ab82c9649f3ea5214efe76
Anders Strangstad <Anders@strangstad.no>
2025-05-21 08:21:24 -0400

    added pylint and formating

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,19 +1,34 @@
+"""
+Package to request from watson-emotion
+"""
 import requests
-import json
 
 def emotion_detector(text_to_analyse: str) -> str:
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    """
+    fucntion that requests from watson-emotion API an emotion respons based on text
+    output is a dict
+    """
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_string = { "raw_document": { "text": text_to_analyse } }
 
-    response = requests.post(url, json=input_string, headers=header)
+    response = requests.post(url, json=input_string, headers=header, timeout=20)
     emotions = pars_response(response)
     emotions["dominant_emotion"] = find_dominant_emotion(emotions)
     return emotions
 
 def pars_response(response_text: str) -> dict:
+    """
+    This funciton takes in a text string, converts it to json
+    And returns a subset of the json file
+    """
     emotions = response_text.json()["emotionPredictions"][0]["emotion"]
     return emotions
 
 def find_dominant_emotion(emotions: dict) -> str:
+    """
+    This function takes in a dict, swaps the values from value to key and key to value.
+    so that we can use the max() function to return the the dominant emotion
+    outputs a string of the dominant emotion
+    """
     return max(zip(emotions.values(), emotions.keys()))[1]
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Executing this function initiates the application of Emotion Detector
+to be executed over the Flask channel and deployed on localhost:5000
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,14 +9,28 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """
+    This code rendeers the index.html template
+    """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+    This code receives the text from the HTML interface and runs emotion_detector
+    over it using emotion_detector() function.
+    The output returens a formated string with emotion scores and the
+    dominate emotion
+    """
     text_to_analyze = request.args.get("textToAnalyze")
-    
     emotion = emotion_detector(text_to_analyze)
-    return f"For the given statement, the system response is 'anger': {emotion['anger']}, 'disgust': {emotion['disgust']}, 'fear': {emotion['fear']}, 'joy': {emotion['joy']}, 'sadness': {emotion['sadness']}, The dominant emotion is {emotion['dominant_emotion']}"
+    return f"""For the given statement, the system response is
+     'anger': {emotion['anger']},
+     'disgust': {emotion['disgust']},
+     'fear': {emotion['fear']}, 
+     'joy': {emotion['joy']}, 
+     'sadness': {emotion['sadness']}, 
+     The dominant emotion is {emotion['dominant_emotion']}"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
